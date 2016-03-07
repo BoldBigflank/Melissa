45d8d1bb2c41be1050bc321feae67684083cfa84
Alex Swan <alex@bold-it.com>
2016-03-06 22:36:22 -0800

    intro and banal

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Intro : MonoBehaviour {
-
+	AsyncOperation a;
 	// Use this for initialization
 	void Start () {
-	
+		a = SceneManager.LoadSceneAsync("FirstScene");
+		a.allowSceneActivation = false;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Debug.Log(SceneManager.GetActiveScene());
+		if(a.isDone){
+			Debug.Log("Done!");
+		} else {
+			Debug.Log("Not Done!");
+		}
 		if(Input.GetKeyDown (KeyCode.Escape)){
 			Application.LoadLevel ("Credits");
 		}
@@ -19,7 +28,8 @@ public class Intro : MonoBehaviour {
 
 		}
 		if(Input.GetKeyDown (KeyCode.X)){
-			Application.LoadLevel("FirstScene");
+//			Application.LoadLevel("FirstScene");
+			a.allowSceneActivation = true;
 		}
 	}
 
